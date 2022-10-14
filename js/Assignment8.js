fd742c88920d236a74454f59870a05fc295967a0
AndrewHartt <89488298+AndrewHartt@users.noreply.github.com>
2022-10-14 12:19:43 -0600

    Assignment8 updated

--- js/Assignment8.js
+++ js/Assignment8.js
@@ -1,5 +1,5 @@
 
-1. COMPLETE VARIABLE AND FUNCTION DEFINITIONS
+
 
 const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
@@ -10,45 +10,57 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-2. RAW TEXT STRINGS
+
 
 var StoryText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 
-const insertX = ['Willy the Goblin','Big Daddy','Father Christmas'];
-let X1 = 'Willy the Goblin';
-let X2 = 'Big Daddy';
-let X3 = 'Father Christmas';
+const insertX = ['The Greek Freak','LeKing','Slim Reaper'];
 
-const insertY = ['the soup kitchen', 'Disneyland','the White House'];
-let Y1 = 'the soup kitchen';
-let Y2 = 'Disneyland';
-let Y3 = 'the White House';
 
-const insertZ = []
-let Z1 = 'spontaneously combusted'
-let Z2 = 'melted into a puddle on the sidewalk'
-let Z3 = 'turned into a slug and crawled away'
+const insertY = ['The Court', 'The Deer District','The Stadium'];
 
-3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
 
-randomize.addEventListener('click', function result(){
-    const initialText = 'click';
-});
+const insertZ = ['got their heads in the game and dropped some easy triple doubles on the opposition','got their heads in the game and posterized some pour souls','got their heads in the game and Crossed Up everyone there with unrivaled moves'];
+
+
+
+
+
+
+
+
+randomize.addEventListener('click', result);
+
+
 
 
 function result() {
 
+    let NewStory = StoryText;
+    let XItem = randomValueFromArray(insertX);
+    let YItem = randomValueFromArray(insertY);
+    let ZItem = randomValueFromArray(insertZ);
+    
+    NewStory = NewStory.replaceAll(':insertx:', XItem);
+    NewStory = NewStory.replaceAll(':inserty:', YItem);
+    NewStory = NewStory.replaceAll(':insertz:', ZItem);
+
   if(customName.value !== '') {
     const name = customName.value;
+    NewStory = NewStory.replace('Bob', name);
+
 
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
+    const weight = Math.round(21.4286) +' stone';
+    const temperature =  Math.round(34.4444)+' centigrade';
+    NewStory = NewStory.replaceAll('94 fahrenheit', weight);
+    NewStory = NewStory.replaceAll('300 pounds', temperature);
+    
 
   }
 
-  story.textContent = ;
+  story.textContent = NewStory;
   story.style.visibility = 'visible';
 }
