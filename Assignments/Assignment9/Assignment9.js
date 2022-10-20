0ebc30cb196b1ddcadc4adbeac606aeeb257fabe
AndrewHartt <89488298+AndrewHartt@users.noreply.github.com>
2022-10-20 12:06:57 -0600

    Assignment 9

--- Assignments/Assignment9/Assignment9.js
+++ Assignments/Assignment9/Assignment9.js
@@ -7,17 +7,18 @@ const overlay = document.querySelector('.overlay');
 /* Declaring the array of image filenames */
 const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 /* Declaring the alternative text for each image file */
-const alts = {'pic1.jpg':'image of human eye', 
-'pic2.jpg':'rock face?', 
-'pic3.jpg':'purple flowers',
-'pic4.jpg':'egyptian drawings', 
-'pic4.jpg':'butterfly'}
+const alts = {
+'pic1.jpg' : 'image of human eye', 
+'pic2.jpg' : 'rock face?', 
+'pic3.jpg' : 'purple flowers',
+'pic4.jpg' : 'egyptian drawings', 
+'pic4.jpg' : 'butterfly'}
 /* Looping through images */
 for(const image of images) {
     const newImage = document.createElement('img');
-    newImage.setAttribute('src', 'images/${image}');
-    newImage.setAttribute('alt', 'alts[images]');
-    thumbBar.appendChild(newImage);
+    newImage.setAttribute('src',  `images/${image}`);
+    newImage.setAttribute('alt', alts[images]);
+    thumb-bar.appendChild(newImage);
     newImage.addEventListener('click', e => {
         displayedImage.src = e.target.src;
         displayedImage.alt = e.target.alt;
@@ -38,3 +39,6 @@ btn.addEventListener('click', ()=> {
         overlay.style.backgroundColor = 'rgba(0,0,0,0,0';
     }
 });
+
+
+
